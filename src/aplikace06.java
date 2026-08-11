f068f22432bda0acd537f612927b37c1ff53bb3e
Karlosando <mracek.k@ssmg.cz>
2026-08-11 22:25:39 +0200

    commit

---
+++ src/aplikace06.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class aplikace06 {
+    public static void main(String[] args){
+        String[] hry = {"Minecraft", "Fortnite", "RDR2"};
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Hledat hru: ");
+        String hledani = sc.next();
+
+        for (String hra : hry) {
+            if (hledani.equals(hra)) {
+                System.out.println("podarilo");
+                return;
+            }
+        }
+        System.out.println("nepodarilo");
+    }
+}
