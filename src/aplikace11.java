3d748b753843b6d059b3bb18888466c516dcc6ae
Karlosando <mracek.k@ssmg.cz>
2026-08-29 10:41:33 +0200

    commit - zkouska

---
+++ src/aplikace11.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class aplikace11 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        displayMenu();
+    }
+    static void displayMenu(){
+        System.out.println("---------------------------------------------");
+        System.out.println("------------------Bankomat-------------------");
+        System.out.println("1-showbalance, 2-deposite, 3-withdraw, 4-exit");
+        System.out.println("---------------------------------------------");
+        }
+
+
+
+        //get and process user choice
+
+        //showbalance
+
+        //deposit
+
+        //withdraw
+
+        //exit
+
+}
